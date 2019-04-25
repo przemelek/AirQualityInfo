43e4b82891166ef9b9613f3a55acaf0897e43f0d
przemelek <przemelek@gmail.com>
2019-04-25 12:07:31 +0200

    Added possibility to determine address where we are (thanks to this if we change place we can see that data is for different address)

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ const http = require('http');
 const fs = require('fs');
 const child = require('child_process');
 
-// You need your own Airly apikey, you may generate one here https://developer.airly.eu/
 const API_KEY = "<YOUR_AIRLY_API_KEY>";
 const AIRLY_LEVELS_TO_IMAGES = {
     "VERY_LOW": "green",
@@ -24,8 +23,12 @@ const AIRLY_LEVELS_TO_IMAGES = {
 
 let tray;
 var location = null;
+const isDarwin = process.platform=="darwin";
+const isLinux = process.platform=="linux";
+const isWin = process.platform=="win32";
 
 const locationDataFile = path.join(app.getPath("userData"), "location.dat");
+const cityDataFile = path.join(app.getPath("userData"), "city.dat");
 
 function getLocation() {
     var location = null;
@@ -35,7 +38,16 @@ function getLocation() {
         console.log(e);
     }
     return location;
+}
 
+function getCity() {
+    var c = null;
+    try {
+      c = fs.readFileSync(cityDataFile);
+    } catch (e) {
+      console.log(e);
+    }
+    return c;
 }
 
 function getLocationFromUser() {
@@ -50,7 +62,7 @@ function getLocationFromUser() {
             console.log(lat + " " + lon);
             location = lat + "," + lon;
             fs.writeFileSync(locationDataFile, "" + location);
-            getAirly();
+            setCity(getAirly);
         }
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('<body><html><script>window.close();</script><H1>You can close this window now :-)</H1></html></body>');
@@ -59,7 +71,14 @@ function getLocationFromUser() {
     });
     server.listen(8000);
     console.log("Starting app");
-    child.execFile('open', ["https://rmk-hrd.appspot.com/file/geoHelper.html"], function (err, data) {
+    var cmd = "open";
+    if (isLinux) {
+      cmd = "xdg-open";
+    }
+    if (isWin) {
+      cmd = "start";
+    }
+    child.execFile(cmd, ["https://rmk-hrd.appspot.com/file/geoHelper.html"], function (err, data) {
         if (err) {
             console.error(err);
             return;
@@ -75,13 +94,16 @@ function startApp() {
     const contextMenu = Menu.buildFromTemplate([]);
 
     tray.setContextMenu(contextMenu);
+    tray.setTitle("Waiting...");
 
     if (location == null) {
         getLocationFromUser();
     } else {
         getAirly();
     }
-    app.dock.hide();
+    if (isDarwin) {
+      app.dock.hide();
+    }
 }
 
 function toMap(ar) {
@@ -93,11 +115,56 @@ function toMap(ar) {
     return m;
 }
 
+function lz(s) {
+  s=""+s;
+  while (s.length<2) s="0"+s;
+  return s;
+}
+
+function setCity(callAfter) {
+  console.log("Toster");
+  var elems = location.toString().split(",");
+  console.log("city from "+elems);
+  var latitude = elems[0] * 1.0;
+  var longitude = elems[1] * 1.0;
+  var url = "https://nominatim.openstreetmap.org/reverse?format=json&lat="+latitude+"&lon="+longitude+"&zoom=18&addressdetails=1";
+  const request = net.request(url);
+  var data = "";
+  request.on("response", (response) => {
+    response.on("data", (chunk) => {
+      data+=chunk;
+    });
+    response.on("end", () => {
+      var resp = JSON.parse(data);
+      console.log(resp);
+      var address = resp["address"];
+      var city = address["city"];
+      var staddress = address["road"];
+      var stnumber = address["house_number"];
+      if (!stnumber) stnumber="";
+      var location="";
+      if (city!=null) {
+        location = city+", "+staddress+" "+stnumber;
+      }
+      fs.writeFileSync(cityDataFile, "" + location);
+    });
+    if (callAfter) {
+      callAfter();
+    }
+  });
+  request.on("error", (error) => {
+    console.log(error);
+    if (callAfter) {
+      callAfter();
+    }
+  });
+  request.end();
+}
+
 function getAirly() {
     var elems = location.toString().split(",");
     var latitude = elems[0] * 1.0;
     var longitude = elems[1] * 1.0;
-    
     const request = net.request({
         method: 'GET',
         protocol: 'https:',
@@ -115,7 +182,8 @@ function getAirly() {
             var current = JSON.parse(data)["current"];
             var values = toMap(current["values"]);
             var indexes = toMap(current["indexes"]);
-            tray.setTitle("" + indexes["AIRLY_CAQI"]);
+            var titleStr = "" + indexes["AIRLY_CAQI"];
+            tray.setTitle(titleStr);
             var level = current["indexes"][0]["level"];
 
             var imgLocation = app.getAppPath() + "/" + AIRLY_LEVELS_TO_IMAGES[level] + ".png";
@@ -123,12 +191,23 @@ function getAirly() {
             tray.setImage(imgLocation);
 
             var content = [];
+            content.push({label: "CAQI: "+indexes["AIRLY_CAQI"]});
             var valuesArray = current["values"];
             for (var i = 0; i < valuesArray.length; i++) {
                 var current = valuesArray[i];
                 content.push({
                     label: current.name + ": " + current.value
                 });
+            }
+            var date = new Date();
+            var time = lz(date.getHours())+":"+lz(date.getMinutes())+":"+lz(date.getSeconds());
+            content.push({label: "TIME: "+time});
+            var city = getCity();
+            console.log("city #"+city+"#"+city.length);
+            if (city!=null && city.length>0) {
+              content.push({label: "ADDRESS: "+city});
+            } else {
+              setCity();
             }
             content.push({type: "separator"});
             content.push({
@@ -145,11 +224,16 @@ function getAirly() {
             const contextMenu = Menu.buildFromTemplate(content);
 
             tray.setContextMenu(contextMenu);
-
             setTimeout(getAirly, 15 * 60 * 1000);
         });
     });
-    request.end()
+    request.on("error", (error) => {
+      console.log(error);
+      // setTimeout(getAirly, 3 * 60 * 1000);
+      setTimeout(getAirly, 1000);
+    });
+    request.end();
+
 }
 
 app.on('ready', startApp);
